5b9c688621d34ad26fa924963307fb3d124114b2
anuvanth <anuvanthsuresh97@gmail.com>
2020-01-18 14:37:08 +0530

    version 1

--- src/main/java/Knight/TO_DO_LIST/To_do_demo.java
+++ src/main/java/Knight/TO_DO_LIST/To_do_demo.java
@@ -12,7 +12,7 @@ public class To_do_demo {
 		Function function = new Function();
 
 		db_operation.insert_Task("test", function.getDate());
-		//db_operation.delete_Task("2");
+		// db_operation.delete_Task("2");
 		//db_operation.Reset_all();
 		db_operation.Get_task();
 		
